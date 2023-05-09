bd8e3c5beec4186050571ee1f421380a8cb48bbe
Darko Kukovec <darko@infinum.com>
2023-05-09 15:51:15 +0200

    Test the SW init

---
+++ src/worker/init.test.ts
@@ -0,0 +1,126 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import { IProvider } from '../interfaces/IProvider';
+import { GrantFlow } from '../shared/enums';
+import { initAuthWorker } from './init';
+import { getState } from './state';
+
+jest.mock('./state', () => ({
+	getState: jest.fn(),
+	saveState: jest.fn(),
+}));
+
+describe('worker/init', () => {
+	describe('initAuthWorker', () => {
+		beforeEach(() => {
+			jest.spyOn(globalThis, 'addEventListener');
+			jest.spyOn(globalThis, 'removeEventListener');
+
+			(getState as jest.Mock).mockResolvedValue({});
+		});
+
+		afterEach(() => {
+			jest.resetAllMocks();
+		});
+
+		it('should work with a custom config param', async () => {
+			const providers: Record<string, IProvider> = {
+				exampleProvider: {
+					grantType: GrantFlow.Token,
+				},
+			};
+
+			const cleanupFn = await initAuthWorker(providers, 'config={"test": 1}');
+
+			expect(cleanupFn).toBeInstanceOf(Function);
+			expect(globalThis.addEventListener).toHaveBeenCalledWith('fetch', expect.any(Function));
+			expect(globalThis.addEventListener).toHaveBeenCalledWith('message', expect.any(Function));
+			expect(globalThis.removeEventListener).not.toHaveBeenCalledWith('fetch', expect.any(Function));
+			expect(globalThis.removeEventListener).not.toHaveBeenCalledWith('message', expect.any(Function));
+
+			cleanupFn();
+			expect(globalThis.removeEventListener).toHaveBeenCalledWith('fetch', expect.any(Function));
+			expect(globalThis.removeEventListener).toHaveBeenCalledWith('message', expect.any(Function));
+
+			const state = await getState();
+
+			expect(state.config).toEqual({
+				config: {
+					test: 1,
+				},
+				debug: false,
+				providers,
+			});
+		});
+
+		it('should work with URL params', async () => {
+			const providers: Record<string, IProvider> = {
+				exampleProvider: {
+					grantType: GrantFlow.Token,
+				},
+			};
+
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			delete globalThis.location;
+			globalThis.location = { search: '?config={"test": 2}' } as Location;
+
+			const cleanupFn = await initAuthWorker(providers);
+
+			expect(cleanupFn).toBeInstanceOf(Function);
+			expect(globalThis.addEventListener).toHaveBeenCalledWith('fetch', expect.any(Function));
+			expect(globalThis.addEventListener).toHaveBeenCalledWith('message', expect.any(Function));
+			expect(globalThis.removeEventListener).not.toHaveBeenCalledWith('fetch', expect.any(Function));
+			expect(globalThis.removeEventListener).not.toHaveBeenCalledWith('message', expect.any(Function));
+
+			cleanupFn();
+			expect(globalThis.removeEventListener).toHaveBeenCalledWith('fetch', expect.any(Function));
+			expect(globalThis.removeEventListener).toHaveBeenCalledWith('message', expect.any(Function));
+
+			const state = await getState();
+
+			expect(state.config).toEqual({
+				config: {
+					test: 2,
+				},
+				debug: false,
+				providers,
+			});
+		});
+
+		it('should work without URL params', async () => {
+			const providers: Record<string, IProvider> = {
+				exampleProvider: {
+					grantType: GrantFlow.Token,
+				},
+			};
+
+			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+			// @ts-ignore
+			delete globalThis.location;
+			globalThis.location = { search: '' } as Location;
+
+			const cleanupFn = await initAuthWorker(providers);
+
+			expect(cleanupFn).toBeInstanceOf(Function);
+			expect(globalThis.addEventListener).toHaveBeenCalledWith('fetch', expect.any(Function));
+			expect(globalThis.addEventListener).toHaveBeenCalledWith('message', expect.any(Function));
+			expect(globalThis.removeEventListener).not.toHaveBeenCalledWith('fetch', expect.any(Function));
+			expect(globalThis.removeEventListener).not.toHaveBeenCalledWith('message', expect.any(Function));
+
+			cleanupFn();
+			expect(globalThis.removeEventListener).toHaveBeenCalledWith('fetch', expect.any(Function));
+			expect(globalThis.removeEventListener).toHaveBeenCalledWith('message', expect.any(Function));
+
+			const state = await getState();
+
+			expect(state.config).toEqual({
+				config: {},
+				debug: false,
+				providers,
+			});
+		});
+	});
+});
---
+++ src/worker/init.ts
@@ -0,0 +1,34 @@
+import { IConfig } from '../interfaces/IConfig';
+import { IProvider } from '../interfaces/IProvider';
+import { fetchListener } from './interceptor';
+import { messageListener } from './postMesage';
+import { getState, saveState } from './state';
+import { log } from './utils';
+
+function getConfig(data: string = location.search.slice(1)) {
+	const params = new URLSearchParams(data);
+	const config = JSON.parse(decodeURIComponent(params.get('config') || '{}')) as IConfig;
+	const debug = params.get('debug') === '1';
+
+	return { config, debug };
+}
+
+export async function initAuthWorker(providers: Record<string, IProvider>, urlConfig?: string): Promise<() => void> {
+	const { config, debug } = getConfig(urlConfig);
+	const state = await getState();
+	state.config = { config, providers, debug };
+	state.providers = providers;
+	saveState();
+
+	const scope = globalThis as unknown as ServiceWorkerGlobalScope;
+
+	log('init', state.config);
+
+	scope.addEventListener('fetch', fetchListener);
+	scope.addEventListener('message', messageListener);
+
+	return () => {
+		scope.removeEventListener('fetch', fetchListener);
+		scope.removeEventListener('message', messageListener);
+	};
+}
