9fa128bcce7ca0ba534f85642789cc0a9d97fa90
Darko Kukovec <darko@infinum.com>
2023-05-09 17:28:06 +0200

    WIP createSession tests

--- src/worker/operations.test.ts
+++ src/worker/operations.test.ts
@@ -1,6 +1,7 @@
 import jwtDecode from 'jwt-decode';
-import { deleteSession, getUserData } from './operations';
+import { createSession, deleteSession, getUserData } from './operations';
 import { getState, saveState } from './state';
+import { GrantFlow } from '../shared/enums';
 
 jest.mock('./state');
 jest.mock('jwt-decode', () => {
@@ -14,6 +15,88 @@ describe('worker/operations', () => {
 
 	beforeEach(() => {
 		jest.spyOn(globalThis, 'fetch');
+	});
+
+	describe('createSession', () => {
+		it('should fail if there is no config', async () => {
+			const state = {};
+
+			(getState as jest.Mock).mockReturnValue(state);
+
+			await expect(createSession('', 'mockProvider', '', 'example.com')).rejects.toThrow('No config found');
+		});
+
+		it('should fail if there is no valid providers', async () => {
+			const state = { config: {} };
+
+			(getState as jest.Mock).mockReturnValue(state);
+
+			await expect(createSession('', 'mockProvider', '', 'example.com')).rejects.toThrow(
+				'No provider params found (createSession)'
+			);
+		});
+
+		it('should fail on invalid state', async () => {
+			const state = {
+				config: {
+					providers: {
+						mockProvider: {},
+					},
+					config: {
+						mockProvider: {},
+					},
+				},
+			};
+
+			(getState as jest.Mock).mockReturnValue(state);
+
+			await expect(createSession('', 'mockProvider', '123', 'example.com')).rejects.toThrow('Invalid state');
+		});
+
+		it('shuld work for token flow', async () => {
+			const state = {
+				session: {
+					provider: 'mockProvider',
+				},
+				config: {
+					providers: {
+						mockProvider: {
+							stateParam: 'state_param',
+							expiresInName: 'expiresIn',
+							accessTokenName: 'access',
+							grantType: GrantFlow.Token,
+							userInfoTokenName: 'user',
+						},
+					},
+					config: {
+						mockProvider: {},
+					},
+				},
+			};
+
+			(getState as jest.Mock).mockResolvedValue(state);
+
+			const result = await createSession(
+				'state_param=123&expiresIn=12&access=mockAccess&user=mockUserInfo&stuff=test',
+				'mockProvider',
+				'123',
+				'example.com'
+			);
+
+			expect(state.session).toEqual({
+				provider: 'mockProvider',
+				accessToken: 'mockAccess',
+				tokenType: 'Bearer',
+				refreshToken: undefined,
+				expiresAt: 12 * 1000 + Date.now(),
+				userInfo: 'mockUserInfo',
+			});
+
+			expect(result).toEqual({
+				provider: 'mockProvider',
+				data: 'mockUserInfo',
+			});
+		});
 	});
 
 	describe('getUserData', () => {
