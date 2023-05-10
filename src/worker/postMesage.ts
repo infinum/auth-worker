41c23a86343a582f788293788157a300f70b876d
Darko Kukovec <darko@infinum.com>
2023-05-10 10:32:01 +0200

    Make the listener sync

--- src/worker/postMesage.ts
+++ src/worker/postMesage.ts
@@ -9,18 +9,20 @@ const operations = {
 	deleteSession,
 } as const;
 
-export async function messageListener(event: ExtendableMessageEvent) {
+export function messageListener(event: ExtendableMessageEvent): void {
 	log('message', event.data.type, event.data.fnName);
 	if (event.data.type === 'call') {
 		if (event.data.fnName in operations) {
 			// eslint-disable-next-line @typescript-eslint/no-explicit-any
 			const fn = operations[event.data.fnName as keyof typeof operations] as any;
-			try {
-				const result = await fn(...event.data.options);
-				event.source?.postMessage({ key: event.data.caller, result });
-			} catch (error) {
-				event.source?.postMessage({ key: event.data.caller, error: (error as Error).message });
-			}
+			fn(...event.data.options).then(
+				(result: unknown) => {
+					event.source?.postMessage({ key: event.data.caller, result });
+				},
+				(error: Error) => {
+					event.source?.postMessage({ key: event.data.caller, error: error.message });
+				}
+			);
 		}
 	}
 }
